2b00dc456b26bb5a0245cf7ec8f7c84dad76b5a1
Dumbledorithm <mishrashardendu3@gmail.com>
2025-10-08 15:18:03 +0530

    fixed hydration error

--- app/components/ExperienceSection.tsx
+++ app/components/ExperienceSection.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import React from 'react';
 import { BentoGridItem } from './BentoGridItem';
 import type { Experience } from '../data/mockData';
--- app/components/OpenToWorkCard.tsx
+++ app/components/OpenToWorkCard.tsx
@@ -1,12 +1,20 @@
-import React from 'react';
+'use client';
+
+import React, { useEffect, useState } from 'react';
 import { BentoGridItem } from './BentoGridItem';
 
 export const FreelanceCard = () => {
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
   return (
     <BentoGridItem className="p-6 flex flex-col items-center justify-center space-y-4">
       <div className="text-center">
         <span className="relative flex h-3 w-3 mb-2 mx-auto">
-          <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-green-400 opacity-75"></span>
+          <span className={`absolute inline-flex h-full w-full rounded-full bg-green-400 opacity-75 ${isClient ? 'animate-ping' : ''}`}></span>
           <span className="relative inline-flex rounded-full h-3 w-3 bg-green-500"></span>
         </span>
         <p className="font-medium text-white">AVAILABLE FOR FREELANCE</p>
